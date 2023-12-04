49b627ba91e1462d3fa2d189651e5246cd726f03
Fillan Swift <fillan.swift@gmail.com>
2023-12-03 17:53:00 -0800

    cleanup
    - improved documentation
    - fixed minor bug where first pair of axes had a rotation speed of zero
    - added ndim, step to CLI
    - minor improvements

--- hypercube.py
+++ hypercube.py
@@ -3,136 +3,157 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib import collections  as mc
 from matplotlib import cm
+import sys
 
 
-def rotate(coords : np.ndarray, theta : float, rot : list[int], dim : int) -> np.ndarray:
+def rotate(vertices : np.ndarray, theta : float, rot : np.ndarray, ndim : int) -> np.ndarray:
+    """
+    Rotate the hypercube in n-ndimensional space
+
+    vertices: static vertices of hypercube
+    theta: angle to rotate by
+    rot: pair of indices representing axis to rotate around
+    ndim: number of dimensions
+    """
     # construct rotation matrix
-    vals = [np.cos(theta),np.sin(theta),-np.sin(theta),np.cos(theta)]
-    rmtx = np.identity(dim)
-    i = -1
-    for x in rot:
-        for y in rot:
-            i += 1
-            rmtx[x,y] = vals[i]
+    vals = np.array([np.cos(theta),np.sin(theta),-np.sin(theta),np.cos(theta)])
+    rmtx = np.identity(ndim)
+    for i, val in enumerate(vals):
+        x = rot[i//2]
+        y = rot[i%2]
+        rmtx[x,y] = val
 
     # transform shape
-    vec = np.transpose(rmtx @ coords.T)
+    vec = np.transpose(rmtx @ vertices.T)
     return vec
 
 
-dim = 5 # number of dimensions
-step = 3
-#rot = [[0,1],[0,2],[0,3],[1,2],[1,3],[2,3]]#[[0,2],[2,3]] # axes to rotate
-rot = [[0,1],[2,3]] # axes to rotate
-disp = 3 # displacement from origin
-dtheta = [0.02,0.04,0.06] # delta theta
-itheta = 0#np.pi / 16 # initial rotation
-irot = [0,1] # initial rotation axes
-
-# generate rotation information
-rot = np.asarray([[i,i+step] for i in range(dim-1)])
-rot[rot >= dim] = 0
-dtheta = [0.03 * i for i in range(dim-1)]
-
-# create displacement vector
-dvec = np.ones(dim) * disp
-dvec[:2] = 0
-
-# construct "unit" hypercube (nvtx * dim)
-nvtx = 2**dim
-nedges = 2**(dim-1)*dim
-vertices = 2 * ((np.arange(2**dim)[:,None] & (1 << np.arange(dim))) > 0) - 1
-
-# find edges
-axcols = cm.rainbow(np.linspace(0, 1, dim))
-#axcols = [[1,0,0,1],[0,1,0,1],[0,0,1,1],[0,0,0,1],[1,0,1,1],[0,1,1,1],[1,1,0,1]]
-ecols = [] # colors of lines (color-coded by axis)
-edges = []#np.zeros((nedges,2)) # indices of vertices on each edge
-for i in range(dim):
-    vtx = list(range(nvtx)) # list of remaining vertices
-    while(len(vtx) > 0):
-        v1 = vtx.pop(0)
-        valid = vtx.copy()
-        # dismiss verties that don't share an axis
-        for j in range(dim):
-            if i == j : valid = [k for k in valid if vertices[v1,j] != vertices[k,j]]
-            else : valid = [k for k in valid if vertices[v1,j] == vertices[k,j]]
-        v2 = valid[0]
-        vtx.remove(v2)
-        edges.append([v1,v2])
-        ecols.append(axcols[i])
-edges = np.asarray(edges)
-ecols = np.asarray(ecols)
-
-# apply initial rotation
-vertices = rotate(vertices,itheta,irot,dim)
-
-
-# render
-fig, ax = plt.subplots()
-
-def update(frame : int, dtheta : list[float], dim : int, disp : float, rot : list[list[int]], dvec : np.ndarray, coords : np.ndarray, edges : np.ndarray, ecols : np.ndarray) -> None:
+def update(frame : int, ndim : int, dtheta : np.ndarray, rot : np.ndarray, disp : float, dvec : np.ndarray, vertices : np.ndarray, edges : np.ndarray, ecols : np.ndarray) -> None:
     '''
-    update the ND shape render; display at new rotation
+    Update the hypercube's rotation
+
+    frame: frame index
+    ndim: number of dimensions
+    dtheta: change in rotation angle per tick along each dimension
+    rot: list of paired indices representing axes to rotate around
+    disp: displacement from origin (used to set axis limits)
+    dvec: displacement vector
+    vertices: static vertices of hypercube
+    edges: list of pairs of vertex indices
+    ecols: list of edge colors
     '''
     
     # rotate shape
-    tcoords = coords[:,:]
-    for i, r in enumerate(rot):
+    vtxs = vertices[:,:] # copy hypercube
+    for i, r in enumerate(rot): # apply 2D rotations sequentially
         theta = frame * dtheta[i]
-        tcoords = rotate(tcoords,theta,r,dim)
-    vec = tcoords[:,:] + dvec
+        vtxs = rotate(vtxs,theta,r,ndim)
 
-    '''# construct rotation matrix
-    theta = frame * dtheta
-    vals = [np.cos(theta),np.sin(theta),-np.sin(theta),np.cos(theta)]
-    rmtx = np.identity(dim)
-    i = -1
-    for x in rot:
-        for y in rot:
-            i += 1
-            rmtx[x,y] = vals[i]
-
-    # transform shape
-    tcoords = np.transpose(rmtx @ coords.T) + dvec
-    vec = tcoords[:,:]'''
+    vec = vtxs[:,:] + dvec # apply offset
 
     # project to 2D
     while vec.shape[1] > 2:
         idx = vec.shape[1] -1
         delta = dvec[idx] / vec[:,idx]
         adjust = np.asarray([delta]).T @ np.ones((1,vec.shape[1]))
-        #print(adjust)
-        #print(vec)
-        vec = vec * adjust
+        vec *= adjust
         vec = vec[:,:idx]
 
     # reset axis
     ax.clear()
-    space = disp * 2
+    space = disp
     ax.set_xlim(-space,space)
     ax.set_ylim(-space,space)
     ax.set_aspect('equal')
 
     # render
     ax.scatter(vec[:,0],vec[:,1],c='k',s=0.75) # draw vertices
-
-    lines = [[tuple(vec[edges[i,0],:]),tuple(vec[edges[i,1],:])] for i in range(edges.shape[0])]
-    '''lcols = []
-    for i in range(edges.shape[0]):
-        z1 = tcoords[edges[i,0],:]
-        z2 = tcoords[edges[i,1],:]
-        r = min(abs(z1[rot[1]]) / disp,1)
-        g = min(abs(z2[rot[1]]) / disp,1)
-        b = 0
-        a = 1
-        lcols.append([r,g,b,a])
-    lcols = np.asarray(lcols)'''
-    #lcols = np.asarray([(1,1,1,tcoords[edges[i]]) for i in range(edges.shape[0])])
-    lc = mc.LineCollection(lines, colors=ecols, linewidths=0.5)
+    lines = [[tuple(vec[edges[i,0],:]),tuple(vec[edges[i,1],:])] for i in range(edges.shape[0])] #
+    lc = mc.LineCollection(lines, colors=ecols, linewidths=0.5) 
     ax.add_collection(lc)
 
 
-ani = animation.FuncAnimation(fig, update, interval=50, fargs=(dtheta,dim,disp,rot,dvec,vertices,edges,ecols))
-plt.show()
-exit(0)
+def get_cli_value(i : int, dft : int, min : int = 0) -> int:
+    """
+    Get integer value from CLI
+
+    i: index of argument
+    dft: default value
+    """
+    if len(sys.argv) > i:
+        try:
+            val = int(sys.argv[i])
+        except:
+            print('Argument {}: Invalid value; cannot convert to integer. Using default'.format(i))
+            return dft
+        if val < min:
+            print('Argument {}: Value must be >= {}! Using default'.format(i,min))
+            return dft
+        return val
+    else:
+        return dft
+
+
+if __name__ == "__main__":
+    # --- Parameters ---
+    ndim = 4 # (default) number of dimensions
+    step = 1 # (default) separation between rotation axis pairs
+    basespd = 0.2 # base rotation speed
+    disp = 2 # displacement from origin
+
+    # --- get inputs from CLI ---
+    # if no CLI argument, uses default values set above
+    ndim = get_cli_value(1,ndim,2)
+    step = get_cli_value(2,step,1)
+
+
+    # --- Construct Hypercube & Initialize Renderer ---
+    # generate rotation information
+    spd = np.round(basespd/ndim,2) # adjusted base rotation speed
+    dtheta = np.array([spd * (i+1) for i in range(ndim-1)])
+    rot = np.array([[i,i+step] for i in range(ndim-1)])
+    rot[rot >= ndim] = 0
+
+    # create displacement vector
+    # this ensures that the hypercube isn't projected onto a plane that intersects it
+    dvec = np.ones(ndim) * disp
+    dvec[:2] = 0 # maintain center on origin in x,y
+
+    # construct unit hypercube centered on origin
+    nvtx = 2**ndim
+    nedges = 2**(ndim-1) * ndim
+    vertices = ((np.arange(nvtx)[:,None] & (1 << np.arange(ndim))) > 0) - 0.5
+
+    # find edges
+    axcols = cm.rainbow(np.linspace(0, 1, ndim))
+    ecols = [] # colors of lines (color-coded by axis)
+    edges = [] # indices of vertices on each edge
+    for i in range(ndim):
+        vtx = list(range(nvtx)) # list of remaining vertices
+        while(len(vtx) > 0):
+            v1 = vtx.pop(0)
+            valid = vtx.copy()
+            # dismiss vertices that don't share an axis
+            for j in range(ndim):
+                if i == j : valid = [k for k in valid if vertices[v1,j] != vertices[k,j]]
+                else : valid = [k for k in valid if vertices[v1,j] == vertices[k,j]]
+            v2 = valid[0]
+            vtx.remove(v2)
+            edges.append([v1,v2])
+            ecols.append(axcols[i])
+    edges = np.asarray(edges)
+    ecols = np.asarray(ecols)
+
+    # apply initial rotation
+    '''
+    itheta = 0 # initial rotation
+    irot = [1,2] # initial rotation axes
+    vertices = rotate(vertices,itheta,irot,ndim)
+    '''
+
+
+    # --- Render ---
+    fig, ax = plt.subplots()
+    ani = animation.FuncAnimation(fig, update, interval=50, fargs=(ndim,dtheta,rot,disp,dvec,vertices,edges,ecols))
+    plt.show()
+    exit(0)
